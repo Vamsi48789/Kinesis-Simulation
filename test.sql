9b8fb0c0f73c9bedde0fc7a7a9279e0c5dccc152
Vamsi48789 <136301666+Vamsi48789@users.noreply.github.com>
2025-08-22 14:37:30 +0530

    test.sql

---
+++ test.sql
@@ -0,0 +1,19 @@
+
+%flink.ssql
+DROP TABLE IF EXISTS sensordata.sensor_data_table;
+
+CREATE TABLE sensor_data_table (
+    sensor_id INT,
+    temperature DOUBLE,
+    humidity DOUBLE,
+    `timestamp` DOUBLE
+) WITH (
+    'connector' = 'kinesis',
+    'stream' = 'SensorData',
+    'aws.region' = 'us-east-1',
+    'scan.stream.initpos' = 'TRIM_HORIZON',
+    'format' = 'json'
+);
+
+%flink.ssql
+SELECT * FROM sensor_data_table;
